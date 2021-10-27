69a2bf0282703200dcfcd31b8a6f5c4d724780c2
Adin Schmahmann <adin.schmahmann@gmail.com>
2021-10-27 12:45:10 -0400

    feat: basic bindnode support

--- app/schema/schema.go
+++ app/schema/schema.go
@@ -47,8 +47,8 @@ var Cmd_Schema = &cli.Command{
 		Usage: "Generate code for working with IPLD schemas",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name : "generator",
-				Usage: "Generator to be used for creating the code. Currently supports (go-gengo)",
+				Name:     "generator",
+				Usage:    "Generator to be used for creating the code. Currently supports (go-gengo, go-bindnode)",
 				Required: true,
 			},
 			&cli.PathFlag{
@@ -124,7 +124,8 @@ func Action_GoCodegen(args *cli.Context) error {
 		return fmt.Errorf("invalid number of arguments")
 	}
 
-	s, err := schemadsl.ParseFile(args.Args().First())
+	schemaFilePath := args.Args().First()
+	s, err := schemadsl.ParseFile(schemaFilePath)
 	if err != nil {
 		return err
 	}
@@ -139,15 +140,46 @@ func Action_GoCodegen(args *cli.Context) error {
 	outputDir := args.Path("output")
 	pkgName := args.String("package")
 
-	if generator != "go-gengo" {
+	switch generator {
+	case "go-gengo":
+		if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
+			return err
+		}
+		a := gengo.AdjunctCfg{}
+		gengo.Generate(outputDir, pkgName, ts, &a)
+	case "go-bindnode":
+		if err := generateGoBindnode(schemaFilePath, outputDir, pkgName, &ts); err != nil {
+			return err
+		}
+	default:
 		return fmt.Errorf("unsupported generator: %s", generator)
 	}
 
+	return nil
+}
+
+func generateGoBindnode(schemaFilePath, outputDir, pkgName string, ts *schema.TypeSystem) error {
 	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
 		return err
 	}
-	a := gengo.AdjunctCfg{}
-	gengo.Generate(outputDir, pkgName, ts, &a)
+
+	// generate types.go
+	f, err := os.Create(fmt.Sprintf("%s/types.go", outputDir))
+	if err != nil {
+		return err
+	}
+
+	if _, err := fmt.Fprintf(f, "package %s\n\n", pkgName); err != nil {
+		return err
+	}
+
+	if err := bindnode.ProduceGoTypes(f, ts); err != nil {
+		return err
+	}
+
+	if err := f.Close(); err != nil {
+		return err
+	}
 
 	return nil
 }
